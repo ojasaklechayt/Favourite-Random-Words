e83f1da48ac703bb79abb73546456ad27e42f765
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-12-13 13:02:44 +0530

    Title of app updates

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Ojas ka First App',
+        title: 'Favourite Radom Words',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
